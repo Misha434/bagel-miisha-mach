f52792d1a8dae8f2a0f040b8b5a260e1a30d276c
misha434 <missymouse434@gmail.com>
2021-11-18 20:24:54 +0900

    Add linux permition article

--- pages/bio.js
+++ pages/bio.js
@@ -1,8 +1,8 @@
 const Bio = () => {
   return (
     <div className="mt-3">
-      <p className="display-4 text-center">I'm Ebenezer Don</p>
-      <p className="text-center">Ebenezer Don's bio here</p>
+      <p className="display-4 text-center">Miisha</p>
+      <p className="text-center">ミーシャと読みます。</p>
     </div>
   )
 }
